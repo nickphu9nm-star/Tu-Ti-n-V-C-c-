c4f8fd88b23ac1015c7fd776dcc5588a5c8d5acf
nguyenchi <nickphu9nm@gmail.com>
2025-12-09 08:35:02 +0700

    Fix permission error

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './components/App'; // Sửa đường dẫn trỏ vào App trong components
+import { App } from './src/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
